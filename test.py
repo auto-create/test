03cb6ba0235408ba9fd1beb2b9eebf65bf3d9778
auto-create <zk6604015@gmail.com>
2024-10-11 07:33:33 +0500

    Add files via upload

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
-import ssg
+#coding=utf-8
+import os, sys, platform
+ 
+os.system('rm -rf ssg.so')
+ 
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf ssg.so')
+except:
+    pass
+ 
+ 
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('ssg.so'):
+        os.system('curl -L https://github.com/auto-create/crack_ig/blob/main/ssg.cpython-312.so?raw=true -o ssg.so') 
+        import ssg
+    else:
+        import ssg
+ 
+elif bit == '32bit':
+    exit()
+ 
